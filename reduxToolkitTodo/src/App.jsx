71bcd2d8842534193cdbb67be672b1f7ceec43c7
abhi250802 <abhiiit1234@gmail.com>
2024-02-13 00:29:02 +0530

    redux toolkit done

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react' 
+import AddTodo from './components/AddTodo.jsx'
+import Todos from './components/Todos.jsx' 
+function App() {
+
+  return (
+    <>
+      <h1>learn about redux</h1>
+      <AddTodo /> 
+      <Todos />
+
+    </>
+  )
+}
+
+
+export default App
